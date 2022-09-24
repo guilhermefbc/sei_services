0469ce59192b808d9e0866efcf5f3f531bce0767
guilhermefbc <gfbc255@gmail.com>
2022-09-24 08:38:25 -0300

    Add basic structure of manual module

---
+++ lib/src/modules/private/manual/presentation/screens/manual_screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ManualScreens extends StatefulWidget {
+  const ManualScreens({Key? key}) : super(key: key);
+
+  @override
+  State<ManualScreens> createState() => _ManualScreensState();
+}
+
+class _ManualScreensState extends State<ManualScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
